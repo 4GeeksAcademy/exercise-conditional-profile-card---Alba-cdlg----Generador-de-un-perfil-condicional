af72d6dc315a9950da40ea95523992d2825dcff9
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-05 08:02:13 +0000

    modificado

--- src/js/app.js
+++ src/js/app.js
@@ -36,8 +36,10 @@ function render(variables = {}) {
           <h1>${variables.name || "Your name"} ${variables.lastName || ""}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+          <ul class=${variables.socialMediaPosition}
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "4geeksacademy"
+            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
